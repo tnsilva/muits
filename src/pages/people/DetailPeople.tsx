7439d62bd36faef45de47b1ffc0cb5c73274db72
tnsilva <tnsilva175@gmail.com>
2022-09-03 19:50:44 -0300

    feat: add VTextField, unform and create/update person

--- src/pages/people/DetailPeople.tsx
+++ src/pages/people/DetailPeople.tsx
@@ -1,13 +1,24 @@
-import { LinearProgress } from "@mui/material";
-import { useEffect, useState } from "react";
+import { LinearProgress, TextField } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DetailTools } from "../../shared/components";
 import { LayoutBasePage } from "../../shared/layouts";
 import { PeopleService } from "../../shared/services/api/people/PeopleService";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import { VTextField } from "../../shared/forms";
+
+interface IFormData {
+  email: string;
+  cityId: number;
+  fullName: string;
+}
 
 export const DetailPeople: React.FC = () => {
   const { id = "new" } = useParams<"id">();
   const navigate = useNavigate();
+
+  const formRef = useRef<FormHandles>(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState("");
@@ -23,14 +34,32 @@ export const DetailPeople: React.FC = () => {
           navigate("/people");
         } else {
           setName(result.fullName);
-          console.log(result);
+          formRef.current?.setData(result);
         }
       });
     }
   }, [id]);
 
-  const handleSave = () => {
-    console.log("save");
+  const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+
+    if (id === "new") {
+      PeopleService.create(dados).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          navigate(`/people/detail/${result}`);
+        }
+      });
+    } else {
+      PeopleService.updateById(Number(id), { id, ...dados }).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+        }
+      });
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -55,15 +84,20 @@ export const DetailPeople: React.FC = () => {
           showSaveAndCloseButton
           showNewButton={id !== "new"}
           showDeleteButton={id !== "new"}
-          clickingOnSave={handleSave}
-          clickingOnSaveAndClose={handleSave}
           clickingOnBack={() => navigate("/people")}
           clickingOnDelete={() => handleDelete(Number(id))}
           clickingOnNew={() => navigate("/people/detail/new")}
+          clickingOnSave={() => formRef.current?.submitForm()}
+          clickingOnSaveAndClose={() => formRef.current?.submitForm()}
         />
       }
     >
-      {isLoading && <LinearProgress variant="indeterminate" />}
+      {/* {isLoading && <LinearProgress variant="indeterminate" />} */}
+      <Form ref={formRef} onSubmit={handleSave}>
+        <VTextField placeholder="Nome completo" name="fullName" />
+        <VTextField placeholder="Email" name="email" />
+        <VTextField placeholder="Cidade" name="cityId" />
+      </Form>
     </LayoutBasePage>
   );
 };
