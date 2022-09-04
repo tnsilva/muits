6c9568a381436da6b6fdd14fd5bd04a93b33c493
tnsilva <tnsilva175@gmail.com>
2022-09-03 23:58:47 -0300

    feat: add yup translations

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { SideMenu } from "./shared/components";
 import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
+import "./shared/forms/YupTranslations";
 
 export const App = () => {
   return (
